1d300a1770c7e1c669fa9abb8ca66ffdf8d604f7
sruthybabu <sruthybabu64@gmail.com>
2022-05-19 21:54:37 +0530

    Navigation completed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addfriend from './components/Addfriend';
 import Viewfriend from './components/Viewfriend';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-
-<Addfriend/>
-<Viewfriend/>
+<BrowserRouter>
+<Routes>
+<Route path="/" exact element={<Addfriend/>}/>
+<Route path="/view" exact element={<Viewfriend/>}/>
+</Routes>
+</BrowserRouter>
 
     </div>
   );
--- src/components/Addfriend.js
+++ src/components/Addfriend.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import Header from './Header'
 
 const Addfriend = () => {
     var [name,setName]=useState("")
@@ -25,6 +26,7 @@ const Addfriend = () => {
     }
   return (
     <div>
+        <Header/>
 <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
